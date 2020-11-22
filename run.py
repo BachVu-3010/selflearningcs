2fc3fe0bbbec22eb7a8f667b65cdb21b6b19b605
Bach Vu <bachvu@1987>
2020-11-22 02:47:39 -0500

    ...

--- run.py
+++ run.py
@@ -7,5 +7,6 @@ from app import create_app, db
 from flask_migrate import Migrate
 
 
-app = create_app(os.getenv("FLASK_CONFIG") or "default")
+# app = create_app(os.getenv("FLASK_CONFIG") or "default")
+app = create_app()
 migrate = Migrate(app, db)
