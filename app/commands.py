2354f0957cdce6f882796151d254770bb16797b9
Bach Vu <bachvu@1987>
2020-11-22 13:03:06 -0500

    .

--- app/commands.py
+++ app/commands.py
@@ -1,10 +1,9 @@
-import click
 from flask.cli import with_appcontext
 
 from . import db
-from .models import User, Question
+from .models import User, Question, Answer
 
-@click.command(name='create_tables')
+
 @with_appcontext
 def create_tables():
     db.create_all()
